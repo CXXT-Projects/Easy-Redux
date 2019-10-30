c1e9c859423287f46eeb8fe0f281efbf389b6df6
luhc228 <luhengchang228@gmail.com>
2019-10-30 17:23:45 +0800

    feat: add connect and mapStateToProps

--- src/Header.js
+++ src/Header.js
@@ -1,33 +1,40 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-
+import { connect } from './react-redux';
 class Header extends Component {
   constructor() {
     super();
     this.state = { themeColor: '' }
   }
 
-  componentDidMount() {
-    this._updateThemeColor();
-    const { store } = this.context;
-    store.subscribe(() => this._updateThemeColor());
-  }
+  // componentDidMount() {
+  //   this._updateThemeColor();
+  //   const { store } = this.context;
+  //   store.subscribe(() => this._updateThemeColor());
+  // }
 
-  _updateThemeColor() {
-    const { store } = this.context;
-    const state = store.getState()
-    this.setState({ themeColor: state.themeColor })
-  }
+  // _updateThemeColor() {
+  //   const { store } = this.context;
+  //   const state = store.getState()
+  //   this.setState({ themeColor: state.themeColor })
+  // }
 
   render() {
     return (
-      <h1 style={{ color: this.state.themeColor }}>Easy-Redux app</h1>
+      <h1 style={{ color: this.props.themeColor }}>Easy-Redux app</h1>
     )
   }
 }
 
-Header.contextTypes = {
-  store: PropTypes.object
+Header.propTypes = {
+  themeColor: PropTypes.string
 }
+
+const mapStateToProps = (state) => {
+  return {
+    themeColor: state.themeColor
+  }
+}
+Header = connect(mapStateToProps)(Header);
 
 export default Header;
